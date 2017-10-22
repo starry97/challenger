4b3d6092fed6be6d87e2a3ec157d748519a3c5e7
Bowen Sun <sunbw@cs.washington.edu>
2017-10-21 22:46:06 -0700

    boto3 working

--- core/main.py
+++ core/main.py
@@ -12,17 +12,30 @@ params = urllib.urlencode({
 # url is from AWS S3.
 body = "{ 'url': 'https://s3-us-west-2.amazonaws.com/dubhacks17/5e19d9d9b51056b8180e34d04f94a74e_Top-Secrets-of-How-to-be-Happy-814-363-c.jpg' }"
 
-try:
-    # NOTE: You must use the same region in your REST call as you used to obtain your subscription keys.
-    #   For example, if you obtained your subscription keys from westcentralus, replace "westus" in the
-    #   URL below with "westcentralus".
-    conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    conn.request("POST", "/emotion/v1.0/recognize?%s" % params, body, headers)
-    response = conn.getresponse()
-    data = response.read()
-    parsed = json.loads(data)
-    print(len(parsed))
-    print(json.dumps(parsed, indent=4, sort_keys=True))
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+s3 = boto3.client(
+    's3',
+    aws_access_key_id='AKIAJFOVQUF3EYLXWPHA',
+    aws_secret_access_key='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'
+)
+
+response = s3.list_buckets()
+
+buckets = [bucket['Name'] for bucket in response['Buckets']]
+
+print("Bucket List: %s" % buckets)
+
+#
+# try:
+#     # NOTE: You must use the same region in your REST call as you used to obtain your subscription keys.
+#     #   For example, if you obtained your subscription keys from westcentralus, replace "westus" in the
+#     #   URL below with "westcentralus".
+#     conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+#     conn.request("POST", "/emotion/v1.0/recognize?%s" % params, body, headers)
+#     response = conn.getresponse()
+#     data = response.read()
+#     parsed = json.loads(data)
+#     print(len(parsed))
+#     print(json.dumps(parsed, indent=4, sort_keys=True))
+#     conn.close()
+# except Exception as e:
+#     print("[Errno {0}] {1}".format(e.errno, e.strerror))
