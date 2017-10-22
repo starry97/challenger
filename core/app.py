0243ff1f4eda4819795cd1ec0bd79debb85d4b78
Xukai Liu <xukail@cs.washington.edu>
2017-10-21 23:00:25 -0700

    app.py address

--- core/app.py
+++ core/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request
 app = Flask(__name__)
+
 @app.route('/', methods=['POST'])
 def result():
+    """
+    Please call url "http://localhost/8000"
+    """
     username = request.form['username']
     input_address = request.form['input_address']
     return 'Received !' # response to your request.
